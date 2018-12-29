d5d446f5c1ffcd4737316575a524907ca1de4cdc
zaq19970105 <1322901630@qq.com>
2018-12-29 09:47:27 +0800

    close #20 complete blueprint

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+git config --global user.email "1322901630@qq.com"
+git config --global user.name "zaq19970105"
+
+git remote add upstream https://github.com/louplus/jobplus9-2
+git pull --rebase upstream master 
+
 sudo pip install -r requirements.txt
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
+
 
